57b2322c49a2342b7df95761c8f41864be5ae512
Victor Kosgei <84603202+vicYegon@users.noreply.github.com>
2022-06-07 18:15:13 +0300

    Update products.py

--- products.py
+++ products.py
@@ -1,85 +1,99 @@
 
 class Products:
-    def __init__(self, product_name, product_id, product_price, product_quantity):
+    def __init__(self, product_name, product_id, product_type, product_price, product_quantity):
         self.product_name = product_name
         self.product_id = product_id
         self.product_price = product_price
         self.product_quantity = product_quantity
-    
+        self.product_type = product_type      
     def __str__(self):
-        self.productdetails =  str(f'{self.product_id} -- {self.product_name} --{self.product_quantity}pieces--Ksh.{self.product_price}each, end=')
-        return self.productdetails
+        self.product_details =  f'{self.product_id} -- {self.product_name} -- {self.product_quantity} -- {self.product_price} -- {self.product_type} \n'
+        return self.product_details
 
 #menu to allow choosing of what product operation to perform
 def product_menu():
-    print("Select what to do with the product entry")
-    print("1. Add product")
-    print("2. Update product")
-    print("3. Delete product")
-    print('4. List all products')
-    selection = int(input("Enter your selection: "))
+    print('''   Select what to do with the product entry
+                1. Add product information in a product list
+                2. Update product details
+                3. Delete product details
+                4. List all products
+    ''')
+    selection = int(input('''\tEnter your selection: '''))
 
     while selection:
         if selection == 1:
-            addProduct()
+            addProductinfo()
         elif selection == 2:
             updateProduct()
         elif selection == 3:
-            deleteProduct()
+            deleteProductinfo()
         elif selection == 4:
             listAllProducts()
         else:
-            print('Wrong input, enter value again')
+            print('\t Wrong input, enter value again')
         break
 
 #adding a product entry to product text file
-def addProduct():
-    product_name = input('Enter product name: ').upper()
-    product_id = input('Enter product_id number: ')
-    product_price = input('Enter product price: ')
+def addProductinfo():
+    with open('products.txt', 'r') as pl:
+        prod_list = pl.readlines()
+        product_id = input('Enter product_id number: ')
+        for line in prod_list:
+            if product_id in line.split('--'):
+                print('Product ID already used')
+                product_menu()
+        product_name = input('Enter product name: ').capitalize()
+        product_type = input('Enter category of the item:').capitalize()
+        product_price = input('Enter product price: ')
+        product_quantity = input('Enter the number of items for the specific product: ')
 
-    p_info = Products(product_name, product_id, product_price)
-    
-    product_details = str(p_info)
+    p_info = Products(product_name, product_id, product_type, product_price, product_quantity,)
 
-    with open('products.txt', 'a', '\n') as f:
-        f_contents = f.write(product_details)
+    with open('products.txt', 'a') as f:
+        f_contents = f.write(p_info.__str__())
         print(f_contents)
     
-
+    product_menu()
 #updating a product entry        
 def updateProduct():
-   with open('products.txt', 'r') as f:
+    with open('products.txt', 'r') as f:
         fileInfo = f.readlines()
         product_id = input("Enter id to update: ")
+        for line in fileInfo:
+            if product_id in line:
+                line_index = fileInfo.index(line)
 
-        new_product_name = input('Enter new or previous name: ').upper()
+        new_product_name = input('Enter new or previous name: ').capitalize()
         new_product_price = input('Enter new product price: ')
-        product_quantity = input('Enter the new number of products after being added: ')
-        new_details = f'{product_id}--{new_product_name}--{new_product_price}--{product_quantity}\n'
+        new_product_quantity = input('Enter the new number of products after being added: ')
+        new_product_type = input('Enter new product category: ').capitalize()
+        new_details = f'{product_id} -- {new_product_name} -- {new_product_price} -- {new_product_quantity} -- {new_product_type}\n'
+    
+    fileInfo[line_index] = new_details 
+    
+    with open('products.txt','w') as fw:
+        for line in fileInfo:
+            fw.write(line)
 
-        for element in fileInfo:
-            if product_id in element:
-                line_index = fileInfo.index(element)
-
-    fileInfo[line_index] = new_details
-
-    with open('products.txt','w') as f:
-        f.writelines(new_details)
 
 #function to delete a product entry
-def deleteProduct():
-    with open('products.txt', 'r') as fr:
-    product_id = input("Enter product id to delete: ")
-    f = fr.readlines()
-    for line in f:
-        if product_id not in line:
-            with open('products.txt', 'w') as fw:
-                fw.write(line)
-print('Product details deleted')
+def deleteProductinfo():
+    with open('products.txt', 'r') as fl:
+        product_id = input("Enter customer id to delete: ")
+        f = fl.readlines()
+        p = 0
+        for line in f:
+            if product_id in line:
+                f.pop(p)
+            p += 1
+    with open('customer.txt', 'w') as fs:
+        for line in f:
+            print(fs.write(line))
 
+    print('Product deleted successfully')
 #appending product entry to a product text file
 def listAllProducts(): 
     with open('products.txt', 'r') as f:
         product_list = f.read()
         print(product_list)
+        

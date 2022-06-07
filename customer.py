c96180eba2cf0b3f14151a3c2095be9cc42f6e71
Victor Kosgei <84603202+vicYegon@users.noreply.github.com>
2022-06-07 17:25:16 +0300

    Update customer.py

--- customer.py
+++ customer.py
@@ -1,5 +1,7 @@
 
 
+
+
 class Customer:
     def __init__(self, customer_id, customer_name, customer_town):
         self.customer_id = customer_id
@@ -10,14 +12,16 @@ class Customer:
         self.customerdetails = f"{self.customer_id}--{self.customer_name}--{self.customer_town}\n"
         return self.customerdetails
     
-#customer operations menu     
+# customer operations menu    
 def customer_menu():
 
-    print("Select what to do with the customer entry: ")
-    print("1. Add customer")
-    print("2. Update customer")
-    print("3. Delete customer")
-    print("4. List all customer entry")
+    print('''
+                Select what to do with the customer entry: 
+                        1. Add customer
+                        2. Update customer            
+                        3. Delete customer
+                        4. List all customer entry
+    ''')
     
     selection = int(input("Enter your selection: "))
     
@@ -28,35 +32,39 @@ def customer_menu():
             updateCustomerinfo()
         elif selection == 3:
             deleteCustomerinfo()
+        elif selection == 4:
+            listAllCustomers()
         else:
+            print('Invalid Option')
             quit()
         break
 
 
-#prints customer list in a new line at the end of the customer.txt file
+# prints customer list in a new line at the end of the customer.txt file
 def addCustomerinfo():
-    with open('customer.txt','r') as fr:
-        customer_id = input('Enter customer ID number: ')
-        cus_list = fr.readlines()
-        for line in cus_list:
-            if cus_id in line.split('--'):
-                print('Customer ID already taken:')
+    with open('customer.txt', 'r') as cus_list:
+        list_items = cus_list.readlines()
+        customer_id = input("Enter customer id number: ")
+        for line in list_items:
+            if str(customer_id) in line.split('--'):
+                print('Customer id already taken')
                 quit()
-        customer_name = input('Enter customer full name: ').upper()
-        customer_town = input('Enter customer town of residence: ').upper()
 
-    c_info = Customer(customer_id, customer_name, customer_town)
+        customer_name = input("Enter customer name: ").upper()
+        customer_address = input("Enter customer town of residence: ").upper()
     
-    customerdetails = str(c_info)
+    c_info = Customer(customer_id, customer_name, customer_address)
 
     with open('customer.txt', 'a') as f:
-        f_contents = f.write(customerdetails)
+        f_contents = f.write(c_info.__str__())
         print(f_contents)
     
+    customer_menu()
+
         
-#function to update customer entry and replace with new details
+# function to update customer entry and replace with new details
 def updateCustomerinfo():
-     with open('customer.txt', 'r') as f:
+    with open('customer.txt', 'r') as f:
         fileInfo = f.readlines()
         cus_id = input("Enter id to update: ")
 
@@ -68,32 +76,35 @@ def updateCustomerinfo():
             if cus_id in element:
                 line_index = fileInfo.index(element)
 
-                fileInfo[line_index] = new_details     
-            
+    fileInfo[line_index] = new_details
+                                 
     with open('customer.txt','w') as fw:
         for line in fileInfo:
             print(fw.write(line))
-            break
+    
+    customer_menu()
 
 #method to remove specified customer entry
 def deleteCustomerinfo():
     with open('customer.txt', 'r') as fl:
-    customer_id = input("Enter customer id to delete: ")
-    f = fl.readlines()
-    j = 0
-    for line in f:
-        if customer_id in line:
-            f.pop(j)
-        j += 1
+        customer_id = input("Enter customer id to delete: ")
+        f = fl.readlines()
+        j = 0
+        for line in f:
+            if customer_id in line:
+                f.pop(j)
+            j += 1
     with open('customer.txt', 'w') as fs:
         for line in f:
             print(fs.write(line))
 
+    print('''
+                    -----Customer deleted successfully-----
+        ''')
 
-#method to read all customer entry in customer file
 def listAllCustomers():
     with open('customer.txt', 'r') as f:
         cus_list = f.read()
         print(cus_list)
-        
-        
+    
+customer_menu()

cd2b8b1cd6269be0a1ed085bc5f0114137b895ca
Victor Kosgei <84603202+vicYegon@users.noreply.github.com>
2022-06-15 03:02:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
-
 from purchases import*
 from customer import*
 from products import*
 
+#menu to call varied menus functions in different files
 def operations_menu():
-    print("Welcome, select what you wish to do: ")
-    print("1. Customer operations")
-    print("2. Product operations")
-    print("3. Order operations")
-    pick = int(input("Enter your selection: "))
-    while pick:
+    while True:
+        print('''
+        \tWelcome to POS, select what you wish to do: \n
+            \t 1. Customer operations
+            \t 2. Product operations
+            \t 3. Order operations
+            \t 0. Quit
+                ''')
+        
+        pick = int(input("\n\t Enter your selection: "))
+    
         if pick == 1:
             customer_menu()
         if pick ==2:
             product_menu()
         if pick == 3:
             order_menu()
+        if pick == 0:
+            quit()
         else:
-            print('Wrong input. Try again')
-            print('If you wish to try again, press 1: ')
-            if 1:
-                operations_menu()
-            else:
-                quit()
-        break
+            print('\tWrong input. Try again or press zero to quit')
 
 if __name__=='__main__':
     operations_menu()

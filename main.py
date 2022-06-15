5bb16256e88db6ce5d2faf1f56a6d32fd50e9a62
Victor Kosgei <84603202+vicYegon@users.noreply.github.com>
2022-06-16 02:45:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from purchases import*
-from customer import*
-from products import*
+from customer import customer_menu
+from products import product_menu
+from query import query_menu
 
 #menu to call varied menus functions in different files
 def operations_menu():
     while True:
         print('''
-        \tWelcome to POS, select what you wish to do: \n
+        \tWelcome to POS system \n\t        Select what you wish to do: \n
             \t 1. Customer operations
             \t 2. Product operations
             \t 3. Order operations
+            \t 4. Queries section
             \t 0. Quit
                 ''')
         
@@ -21,6 +23,8 @@ def operations_menu():
             product_menu()
         if pick == 3:
             order_menu()
+        if pick == 4:
+            query_menu()
         if pick == 0:
             quit()
         else:
